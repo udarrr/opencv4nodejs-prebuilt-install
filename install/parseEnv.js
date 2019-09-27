197956301216e4dadf139193c242dda4bb70a364
Simon Hofmann <simon.hofmann@consol.de>
2019-09-27 15:36:58 +0200

    Using defaults as additional fallback

--- install/parseEnv.js
+++ install/parseEnv.js
@@ -1,7 +1,8 @@
+const defaults = require("./defaults");
 const envName = process.argv[2]
 
 if (!envName) {
   throw new Error('no env name passed to parseEnv')
 }
-const outputs = (process.env[envName] || '').split('\n')
+const outputs = (process.env[envName] || defaults[envName]() || '').split('\n')
 outputs.forEach(o => console.log(o))
