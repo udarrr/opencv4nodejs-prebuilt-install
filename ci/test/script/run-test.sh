9ef265fe668084d375b55567214e9c465bd274f5
Simon Hofmann <simon.hofmann@consol.de>
2019-09-27 09:15:32 +0200

    Updated test path

--- ci/test/script/run-test.sh
+++ ci/test/script/run-test.sh
@@ -3,7 +3,7 @@ export OPENCV4NODEJS_DISABLE_AUTOBUILD=1
 echo installing &&\
 npm install --unsafe-perm &&\
 echo running tests &&\
-cd ../../test &&\
+cd ./test &&\
 npm install --unsafe-perm &&\
 npm test &&\
 npm run test-externalMemTracking
