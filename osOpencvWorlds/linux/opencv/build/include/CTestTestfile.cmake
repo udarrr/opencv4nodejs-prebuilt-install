63a113a32062ef021d90fc41d7a9801b78a73580
udarrr <sergey.klyshnikov2@gmail.com>
2023-06-03 13:41:52 +0300

    added compiled  options

---
+++ osOpencvWorlds/linux/opencv/build/include/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/travis/build/nut-tree/npm-opencv-build/opencv/opencv/include
+# Build directory: /home/travis/build/nut-tree/npm-opencv-build/opencv/build/include
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ osOpencvWorlds/win32/opencv/build/include/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: D:/JavaScript/npm-opencv-build/opencv/opencv/include
+# Build directory: D:/JavaScript/npm-opencv-build/opencv/build/include
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
