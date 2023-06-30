d5c5cc75e797822d6bcca430ce430191c9a81df0
udarrr <siarhei_kliushnikau@epam.com>
2023-07-01 00:07:51 +0300

    v183

--- osOpencvWorlds/win32/opencv/build/include/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/JavaScript/npm-opencv-build/opencv/opencv/include
-# Build directory: D:/JavaScript/npm-opencv-build/opencv/build/include
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
