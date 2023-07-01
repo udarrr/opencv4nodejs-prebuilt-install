452424e5d72528134864a9c7d1ffcd2b1aa79598
udarrr <siarhei_kliushnikau@epam.com>
2023-07-01 13:17:56 +0300

    v193

--- install/download.js
+++ install/download.js
@@ -5,22 +5,40 @@ const Downloader = require("nodejs-file-downloader");
 const packageJson = require("../package.json");
 const path = require("path");
 const file = `opencv_${process.platform}_${packageJson.opencv4nodejs.autoBuildOpencvVersion}_${process.arch}.tgz`;
+console.log(file);
+
 async function download() {
   let libUrl = `https://github.com/udarrr/opencv4nodejs-prebuilt-install/releases/download/v${packageJson.version}/${file}`;
-  let dir = path.join(process.cwd(), "osOpencvWorlds", "win32");
 
-  const downloader = new Downloader({
-    url: libUrl,
-    directory: dir,
-  });
   try {
-    await downloader.download();
-    if (process.platform === "darwin") {
-      pack_1.Pack.unpack(`${path.join(process.cwd(), "osOpencvWorlds", "darwin")}`, `${path.join(process.cwd(), "osOpencvWorlds", "darwin", file)}`);
+    if (process.platform === "darwin" && process.arch !== "arm64") {
+      await new Downloader({
+        url: libUrl,
+        directory: path.join(process.cwd(), "osOpencvWorlds", "darwin"),
+      }).download();
+
+      await pack_1.Pack.unpack(`${path.join(process.cwd(), "osOpencvWorlds", "darwin")}`, `${path.join(process.cwd(), "osOpencvWorlds", "darwin", file)}`);
+    } else if (process.platform === "darwin" && process.arch === "arm64") {
+      await new Downloader({
+        url: libUrl,
+        directory: path.join(process.cwd(), "osOpencvWorlds", "darwinM1"),
+      }).download();
+
+      await pack_1.Pack.unpack(`${path.join(process.cwd(), "osOpencvWorlds", "darwinM1")}`, `${path.join(process.cwd(), "osOpencvWorlds", "darwinM1", file)}`);
     } else if (process.platform === "linux") {
-      pack_1.Pack.unpack(`${path.join(process.cwd(), "osOpencvWorlds", "linux")}`, `${path.join(process.cwd(), "osOpencvWorlds", "linux", file)}`);
+      await new Downloader({
+        url: libUrl,
+        directory: path.join(process.cwd(), "osOpencvWorlds", "linux"),
+      }).download();
+
+      await pack_1.Pack.unpack(`${path.join(process.cwd(), "osOpencvWorlds", "linux")}`, `${path.join(process.cwd(), "osOpencvWorlds", "linux", file)}`);
     } else if (process.platform === "win32") {
-      pack_1.Pack.unpack(`${path.join(process.cwd(), "osOpencvWorlds", "win32")}`, `${path.join(process.cwd(), "osOpencvWorlds", "win32", file)}`).then(() => console.log("done"));
+      await new Downloader({
+        url: libUrl,
+        directory: path.join(process.cwd(), "osOpencvWorlds", "win32"),
+      }).download();
+
+      await pack_1.Pack.unpack(`${path.join(process.cwd(), "osOpencvWorlds", "win32")}`, `${path.join(process.cwd(), "osOpencvWorlds", "win32", file)}`);
     }
     await new Promise((res) =>
       setTimeout(() => {
